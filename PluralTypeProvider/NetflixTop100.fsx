63648592456c703b2d46ab78dccbbf11bb7206b1
Horia Toma <toma.horia@gmail.com>
2016-03-28 19:44:54 +0200

    First version

---
+++ PluralTypeProvider/NetflixTop100.fsx
@@ -0,0 +1,34 @@
+﻿#r "../packages/FSharp.Data.2.2.5/lib/net40/Fsharp.Data.dll"
+#r "System.Xml.Linq.dll"
+
+open FSharp.Data
+open System.Text.RegularExpressions
+
+let regexThumb = 
+    Regex("<a[^>]*><img src=\"([^\"]*)\".*>(.*)")
+
+type Netflix = 
+    XmlProvider<"http://dvd.netflix.com/Top100RSS">
+
+type MovieBasics = 
+    { Title: string
+      Summary: string
+      Thumbnail: option<string> }
+
+let top = Netflix.GetSample()
+top.Channel.Title
+
+let getTop100() = 
+    [ for it in top.Channel.Items ->
+        let r = regexThumb.Match(it.Description)
+        let desc, thumb = 
+            if r.Success then
+                r.Groups.[2].Value,
+                Some(r.Groups.[1].Value)
+            else
+                it.Description, None
+        { Title = it.Title
+          Summary = desc
+          Thumbnail = thumb}]
+
+getTop100()
---
+++ PluralTypeProvider/WorldBank.fsx
@@ -0,0 +1,12 @@
+﻿#r "../packages/FSharp.Data.2.2.5/lib/net40/Fsharp.Data.dll"
+
+open FSharp.Data
+
+let wb = WorldBankData.GetDataContext()
+wb.Countries.Romania.CapitalCity
+wb.Countries.Belgium.Indicators.``Population in largest city``.[2013]
+
+for country in wb.Regions.``Arab World``.Countries do
+    let gdp = country.Indicators.``GDP, PPP (current international $)``.[2014]
+    let gdpBil = gdp / 1.0e9
+    printfn "%s: %.2f" country.Name gdpBil
