2746ebab8979229152f1d263f3080f3bbf04957d
Horia Toma <toma.horia@gmail.com>
2016-03-30 22:54:03 +0200

    Added Library project that uses code from scripts

---
+++ Library/Domain.fs
@@ -0,0 +1,33 @@
+﻿namespace MovieData
+
+type MovieBasics = 
+ { Title: string
+   Summary: string
+   Thumbnail: option<string> }
+
+type Review = 
+    { Published: System.DateTime
+      Summary: string
+      Link: string
+      LinkText: string}
+
+type Cast = 
+    { Actor: string
+      Character: string }
+
+type Details = 
+    { Homepage: string
+      Genres: seq<string>
+      Overview: string
+      Companies: seq<string>
+      Poster: string
+      Countries: seq<string>
+      Released: System.DateTime
+      AverageVote: decimal }
+
+type Movie = 
+    { Movie: MovieBasics
+      Details: Details
+      Cast: seq<Cast>
+      Review: option<Review> }
+
---
+++ Library/Netflix.fs
@@ -0,0 +1,28 @@
+﻿module Netflix
+
+open FSharp.Data
+open System.Text.RegularExpressions
+
+let regexThumb = 
+    Regex("<a[^>]*><img src=\"([^\"]*)\".*>(.*)")
+
+type Netflix = 
+    XmlProvider<"http://dvd.netflix.com/Top100RSS">
+
+type MovieBasics = 
+    { Title: string
+      Summary: string
+      Thumbnail: option<string> }
+
+let getTop100() = 
+    [ for it in Netflix.GetSample().Channel.Items ->
+        let r = regexThumb.Match(it.Description)
+        let desc, thumb = 
+            if r.Success then
+                r.Groups.[2].Value,
+                Some(r.Groups.[1].Value)
+            else
+                it.Description, None
+        { Title = it.Title
+          Summary = desc
+          Thumbnail = thumb}]
