9a1dfe45ac759bb887b67313e16aee1be9bbd114
Horia Toma <toma.horia@gmail.com>
2016-04-10 23:50:40 +0200

    Added Web Project

---
+++ Movies.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Movies.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Index",
+                url: "",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Details",
+                url: "details/{title}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Details",
+                    title = UrlParameter.Optional
+                });
+        }
+    }
+}
---
+++ Movies.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using static Movie.Data.MovieMain;
+
+
+namespace Movies.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var movies = await GetLatestMovies();
+            return View(movies);
+        }
+
+        public async Task<ActionResult> Details(string title)
+        {
+            var details = await GetMovieInfo(title);
+            if (details.HasMovie)
+            {
+                return View(details.Movie);
+            }
+            return View("MovieNotFound");
+        }
+    }
+}
